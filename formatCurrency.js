c2a4873613814f29182edab72ea40bbd38cf2eab
Natanael Junio <natanael_junio@hotmail.com>
2024-10-07 16:30:24 -0300

    Add CI with GitHub Actions

---
+++ formatCurrency.js
@@ -0,0 +1,16 @@
+function formatCurrency(value) {
+  const signal = Number(value) < 0 ? "-" : "";
+
+  value = String(value).replace(/\D/g, "");
+
+  value = Number(value) / 100;
+
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return signal + value;
+}
+
+module.exports = formatCurrency;
---
+++ formatCurrency.test.js
@@ -0,0 +1,20 @@
+// formatCurrency.test.js
+const formatCurrency = require('./formatCurrency');
+
+describe('formatCurrency', () => {
+  test('deve formatar valores positivos corretamente', () => {
+    expect(formatCurrency(12345)).toBe('R$ 123,45');
+  });
+
+  test('deve formatar valores negativos corretamente', () => {
+    expect(formatCurrency(-9876)).toBe('-R$ 98,76');
+  });
+
+  test('deve formatar valores grandes corretamente', () => {
+    expect(formatCurrency(123456789)).toBe('R$ 1.234.567,89');
+  });
+
+  test('deve formatar zero corretamente', () => {
+    expect(formatCurrency(0)).toBe('R$ 0,00');
+  });
+});
